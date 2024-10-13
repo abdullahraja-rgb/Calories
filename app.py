b4f93008d097b150e276e853de4238c082ce7ae7
abdullahraja-rgb <abra73584@gmail.com>
2024-10-13 20:01:13 +0000

    html.txt, app.py

---
+++ app.py
@@ -0,0 +1,64 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+
+from datetime import datetime, timezone
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if (request.method == "POST"):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirmation = request.form.get('confirmation')
+
+        if not username:
+            return apology("Username is required!")
+        elif not password:
+            return apology("Password is required!")
+        elif not confirmation:
+            return apology("Password confirmation is required!")
+        if password != confirmation:
+            return apology("Passwords are not match!")
+
+        hash = generate_password_hash(password)
+        try:
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
+            return redirect('/')
+        except:
+            return apology("Username has already been registered!")
+    else:
+        return render_template("register.html")
+
